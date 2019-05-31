8112ef770fd1d13ab8cf338bbc9280110afd7b6e
unknown <giri.nabin1994@gmail.com>
2019-05-31 19:41:07 +0545

    first commit

---
+++ website/blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
---
+++ website/users/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render,redirect
+from users.forms import Registrationform
+from django.contrib.auth import login,authenticate
+from django.contrib import auth
+
+def register(request):
+    if request.method == 'POST':
+        form = Registrationform(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+        return redirect('blog-home')
+    else:
+        form = Registrationform()
+    return render(request, 'users/register.html', {'form': form})
+
+def logout(request):
+    return render(request, 'registration/logout.html')
+
+def profile(request):
+    return render(request, 'registration/profile.html')
